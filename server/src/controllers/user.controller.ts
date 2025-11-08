2bf6cac2c5a6b8c4a00ef1f5fd57c65fab7e5320
Kiran <116376429+kiranvshah@users.noreply.github.com>
2025-11-08 18:29:49 +0000

    implement GET /users/:userId/historical-perk-transactions

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -48,3 +48,45 @@ export const getUser = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Failed to retrieve user info" });
   }
 };
+
+/**
+ * --- GET /users/:userId/historical-perk-transactions ---
+ * Returns a user's past perk transactions
+ */
+export const getUserHistoricalPerkTransactions = async (req: Request, res: Response) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorised (controller)" });
+    }
+
+    // get the user ID from the JWT token
+    const tokenUserId = req.user.userId;
+
+    // get the user ID from the URL
+    const urlUserId = Number(req.params.userId);
+
+    // security check: make sure the authenticated user is the one they are asking for
+    if (tokenUserId !== urlUserId) {
+      return res
+        .status(403)
+        .json({ error: "Forbidden: you can only view your own user info" });
+    }
+
+    // fetch the data
+    const perkTransactions = await prisma.perkTransaction.findMany({
+      where: {
+        user_id: urlUserId
+      },
+      select: {
+        id: true,
+        date_time: true,
+        perk: true,
+      }
+    })
+
+    res.status(200).json(perkTransactions);
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({error: "Failed to retrieve historical perk transactions"})
+  }
+}
